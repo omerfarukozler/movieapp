ecf0067a999a027e469173675aabadc0ed94d8f9
Ömer Faruk Özler <95633424+omerfarukozler@users.noreply.github.com>
2024-09-07 19:33:58 +0300

    Header and DarkMode Added

---
+++ src/app/layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./global.css"
+import Header from './components/Header'
+import Providers from './Providers'
+const layout = ({children}) => {
+  return (
+  <html lang='en'>
+  <body>
+    <Providers>
+  <Header/>
+    {children} 
+ </Providers>
+  </body>
+
+  </html>
+  )
+}
+export default layout
